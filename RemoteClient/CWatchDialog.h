81bc0e86e2d095f75e8becd557780432a408cde5
历 黄 <1971123505@qq.com>
2022-04-27 21:47:05 +0800

    1、设计了鼠标远程控制的结构 2、实现了本地鼠标信息的获取、封装、发送

--- RemoteClient/CWatchDialog.h
+++ RemoteClient/CWatchDialog.h
@@ -21,8 +21,18 @@ protected:
 
 	DECLARE_MESSAGE_MAP()
 public:
+	// 坐标转换
+	CPoint UserPoint2RemoteScreenPoint(CPoint& point);
 	virtual BOOL OnInitDialog();
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
 	// 图片显示框
 	CStatic m_picture;
+	afx_msg void OnLButtonDblClk(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonDblClk(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnStnClickedWatch();
 };
